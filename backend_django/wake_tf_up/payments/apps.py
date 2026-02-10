02c98a60f6aeff81e730f918059d648f886bbd9a
gether <pat.kredatus@gmail.com>
2026-02-10 12:53:57 +0100

    .

--- backend_django/wake_tf_up/payments/apps.py
+++ backend_django/wake_tf_up/payments/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
-    verbose_name = '06. Payments'
+    verbose_name = 'Payments'
