18cc40bb4ddf14ce3db285fbbe416dbb64b9abdd
gether <pat.kredatus@gmail.com>
2026-03-17 11:14:28 +0100

    .

--- backend_django/wake_tf_up/payments/gopay_service.py
+++ backend_django/wake_tf_up/payments/gopay_service.py
@@ -116,8 +116,6 @@ class GoPayService:
         
         payment_data = {
             "payer": {
-                "default_payment_instrument": "BANK_ACCOUNT",
-                "allowed_payment_instruments": ["BANK_ACCOUNT", "PAYMENT_CARD", "GPAY", "APPLE_PAY"],
                 "contact": {
                     "first_name": order.shipping_name.split()[0] if order.shipping_name else "Customer",
                     "last_name": " ".join(order.shipping_name.split()[1:]) if len(order.shipping_name.split()) > 1 else "",
